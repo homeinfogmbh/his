43bb704463a90e8b9f7e2ca3b79dd6380c688f78
Richard Neumann <r.neumann@homeinfo.de>
2014-09-08 14:56:13 +0200

    foo

--- cream/__init__.py
+++ cream/__init__.py
@@ -2,3 +2,12 @@
 Common Real Estate API, Modified
 """
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
+
+from openimmofs import ITF as OpenImmoFS
+
+#===============================================================================
+# Main index method
+#===============================================================================
+def index(req): 
+    with open('/var/www/de/homeinfo/api/openimmo-data_127.xml', 'r', encoding='latin1') as oi_xml:
+        xmldata = oi_xml.read()

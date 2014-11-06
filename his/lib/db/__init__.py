d7451ca0be56f232f5024a881f5dc0fbce40c6f0
Richard Neumann <mail@richard-neumann.de>
2014-11-06 14:06:02 +0100

    Simplified DB

--- his/lib/db/__init__.py
+++ his/lib/db/__init__.py
@@ -1,5 +1,11 @@
 """
-Database definition for HIS
+Models for the HOMEINFO Integrated Services
 """
+from .passwd import User, Group, GroupMembers
+from .services import Service, UserService, GroupService
+
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
 __date__ = '16.10.2014'
+__tables__ = [User, Group, GroupMembers, 
+              Service, UserService, GroupService]
+"""The actual databases's tables"""
---
+++ his/lib/db/abc.py
@@ -0,0 +1,22 @@
+"""
+Basic HIS database definitions
+"""
+from peewee import Model
+from playhouse.pool import PooledMySQLDatabase
+
+__author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
+__date__ = '09.10.2014'
+__all__ = ['HISModel']
+
+DB = 'his'
+HOST = 'mysql.homeinfo.de'
+USER = 'his'
+PASSWD = '3=w,&7>_u8}oO0y'
+
+class HISModel(Model):
+    """
+    Generic HOMEINFO Integrated Service database model
+    """
+    class Meta:
+        database = PooledMySQLDatabase('his', host=HOST, user=USER, 
+                                       passwd=PASSWD, threadlocals=True)

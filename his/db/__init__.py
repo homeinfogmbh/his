e0b35938139398539e851708fe2e766ca53cefaa
Richard Neumann <r.neumann@homeinfo.de>
2014-12-02 12:13:39 +0100

    fix

--- his/db/__init__.py
+++ his/db/__init__.py
@@ -7,5 +7,5 @@ from .service import Service, UserService, GroupService
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
 __date__ = '16.10.2014'
 __all__ = ['User', 'Group', 'Service', 'UserService', 'GroupService']
-__tables__ = [User, Group, Service, UserService, GroupService]
+__tables__ = [Group, User, Service, GroupService, UserService]
 """The actual databases's tables"""

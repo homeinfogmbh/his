8c200233b4b3cb88ad79b5f216cdc09019cd3b74
Richard Neumann <r.neumann@homeinfo.de>
2014-12-02 11:50:31 +0100

    Working...

--- his/db/abc.py
+++ his/db/abc.py
@@ -1,17 +1,12 @@
 """
 Basic HIS database definitions
 """
-from peewee import Model
-from playhouse.pool import PooledMySQLDatabase
+from his.config import db
+from peewee import Model, MySQLDatabase
 
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
 __date__ = '09.10.2014'
 __all__ = ['HISModel']
-
-DB = 'homeinfo_his'
-HOST = 'mysql.homeinfo.de'
-USER = 'homeinfo_his'
-PASSWD = '3=w,&7>_u8}oO0y'
 
 
 class HISModel(Model):
@@ -19,5 +14,6 @@ class HISModel(Model):
     Generic HOMEINFO Integrated Service database model
     """
     class Meta:
-        database = PooledMySQLDatabase('his', host=HOST,
-                                       user=USER, passwd=PASSWD)
+        database = MySQLDatabase('his', host=db.get('HOST'),
+                                 user=db.get('USER'),
+                                 passwd=db.get('PASSWD'))

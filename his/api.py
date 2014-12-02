987dead4cbd384bde1c19c024e31b4c18b72284f
Richard Neumann <r.neumann@homeinfo.de>
2014-12-02 11:54:52 +0100

    Fix

--- his/api.py
+++ his/api.py
@@ -2,17 +2,17 @@
 Defines the HIS service databases
 """
 from his.config import db
-from playhouse.pool import PooledMySQLDatabase
+from peewee import MySQLDatabase
 
 __date__ = '06.11.2014'
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
 __all__ = ['HISServiceDatabase']
 
 
-class HISServiceDatabase(PooledMySQLDatabase):
+class HISServiceDatabase(MySQLDatabase):
     """A HIS service database"""
-    def __init__(self, database, host=None, user=None,
-                 passwd=None, threadlocals=True, **kwargs):
+    def __init__(self, service, host=None, user=None,
+                 passwd=None, threadlocals=False, **kwargs):
         if host is None:
             host = db.get('host')
         if user is None:
@@ -20,6 +20,6 @@ class HISServiceDatabase(PooledMySQLDatabase):
         if passwd is None:
             passwd = db.get('passwd')
         """Changes the name to create a '_'-separated namespace"""
-        super().__init__('_'.join([db.get('master_db'), database]),
+        super().__init__('_'.join([db.get('master_db'), str(service)]),
                          host=host, user=user, passwd=passwd,
                          threadlocals=threadlocals, kwargs)

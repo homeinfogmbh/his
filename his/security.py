1885718264e4f3b5882a3d6209cd7bca3a29dda4
Richard Neumann <r.neumann@homeinfo.de>
2014-12-05 11:24:59 +0100

    fix

--- his/security.py
+++ his/security.py
@@ -9,81 +9,57 @@ __date__ = '04.12.2014'
 __all__ = ['login', 'authenticate', 'authorize']
 
 
-class NoSuchUser(Exception):
-    """Indicates non existant user"""
-    pass
-
-
-class NoSessionToken(Exception):
-    """Indicates an invalid session token"""
-    pass
-
-
-class NoPassword(Exception):
-    """Indicates an invalid password"""
-
-
 def authenticate(func):
     """Authenticate for a method"""
-    def login(*args, user_name=None, user_pass=None, **kwargs):
+    def _login(user_name, user_pass, *args, **kwargs):
         """Create a session for a user"""
-        if user_name is None:
-            raise NoSuchUser()
-        elif user_pass is None:
-            raise NoPassword()
-        else:
-            user = User.by_user_name(user_name)
-            if user is None:
-                raise NoSuchUser()
-            elif user.locked:
-                raise UserLocked()
-            elif user.passwd == user_pass:
-                for session in Session.select().limit(1).where(Session.user
-                                                               == user):
-                    if session.valid:
-                        raise SessionExists()
-                    else:
-                        session.terminate()
+        user = User.by_user_name(user_name)
+        if user is None:
+            raise InvalidCredentials()
+        elif user.locked:
+            raise UserLocked()
+        elif user.passwd == user_pass:
+            for session in Session.select().limit(1).where(Session.user
+                                                           == user):
+                if session.valid:
+                    raise SessionExists()
                 else:
-                    session = Session.start(user)
-                    user.failed_logins = 0
-                    user.save()
-                    result = func(*args, **kwargs)
-                    result.session_token = session.token
-                    return result
+                    session.terminate()
             else:
-                user.failed_logins += 1
+                session = Session.start(user)
+                user.failed_logins = 0
                 user.save()
-                raise InvalidCredentials()
-
-    def session(*args, user_name=None, session_token=None, **kwargs):
-        """Refresh a session for a user"""
-        if user_name is None:
-            raise NoSuchUser()
-        elif session_token is None:
-            raise NoSessionToken()
+                result = func(*args, **kwargs)
+                result.session_token = session.token
+                return result
         else:
-            user = User.by_user_name(user_name)
-            if user is None:
-                raise NoSuchUser()
-            elif user.locked:
-                raise UserLocked()
-            else:
-                for session in Session.select().limit(1).where(Session.user
-                                                               == user):
-                    if session.valid:
-                        if session.token == session_token:
-                            session = session.refresh()
-                            result = func(*args, **kwargs)
-                            result.session_token = session.token
-                            return result
-                        else:
-                            raise InvalidCredentials()
+            user.failed_logins += 1
+            user.save()
+            raise InvalidCredentials()
+
+    def _session(user_name, session_token, *args, **kwargs):
+        """Refresh a session for a user"""
+        user = User.by_user_name(user_name)
+        if user is None:
+            raise InvalidCredentials()
+        elif user.locked:
+            raise UserLocked()
+        else:
+            for session in Session.select().limit(1).where(Session.user
+                                                           == user):
+                if session.valid:
+                    if session.token == session_token:
+                        session = session.refresh()
+                        result = func(*args, **kwargs)
+                        result.session_token = session.token
+                        return result
                     else:
-                        session.terminate()
-                        raise SessionTimeout()
+                        raise InvalidCredentials()
                 else:
-                    raise NotLoggedIn()
+                    session.terminate()
+                    raise SessionTimeout()
+            else:
+                raise NotLoggedIn()
 
     def authenticate(*args, user_name=None, session_token=None,
                      user_pass=None, **kwargs):
@@ -95,11 +71,11 @@ def authenticate(func):
                 if user_pass is None:
                     raise InvalidCredentials()
                 else:
-                    return login(*args, user_name=user_name,
-                                 user_pass=user_pass, **kwargs)
+                    return _login(*args, user_name=user_name,
+                                  user_pass=user_pass, **kwargs)
             else:
-                return session(*args, user_name=user_name,
-                               session_token=session_token, **kwargs)
+                return _session(*args, user_name=user_name,
+                                session_token=session_token, **kwargs)
 
     return authenticate
 
@@ -121,7 +97,7 @@ def authorize(func):
             raise NotAuthorized()
         user = User.by_user_name(user_name)
         if user is None:
-            raise NoSuchUser()
+            raise InvalidCredentials()
         elif user.locked:
             raise UserLocked()
         else:

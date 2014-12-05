cf329fd6b9076123f8d92d6d6f2cc825294afaa4
Richard Neumann <r.neumann@homeinfo.de>
2014-12-05 11:13:47 +0100

    working

--- his/security.py
+++ his/security.py
@@ -9,69 +9,104 @@ __date__ = '04.12.2014'
 __all__ = ['login', 'authenticate', 'authorize']
 
 
-def login(func):
-    """Login for a method"""
-    def _login(*args, user_name=None, user_pass=None, **kwargs):
-        """Login a user"""
-        if user_name is None or user_pass is None:
-            raise NotAuthenticated()
-        user = User.by_hashed_name(user_name)
-        if user is None:
-            raise NoSuchUser()
-        elif user.locked:
-            raise UserLocked()
-        elif user.passwd == user_pass:
-            for _ in Session.select().limit(1).where(Session.user == user):
-                raise SessionExists()
-            else:
-                user.failed_logins = 0
-                user.save()
-                session = Session.start(user)
-                result = func(*args, **kwargs)
-                result.session_token = session.token
-                return result
-        else:
-            user.failed_logins += 1
-            user.save()
-            raise InvalidCredentials()
+class NoSuchUser(Exception):
+    """Indicates non existant user"""
+    pass
 
-    return _login
+
+class NoSessionToken(Exception):
+    """Indicates an invalid session token"""
+    pass
+
+
+class NoPassword(Exception):
+    """Indicates an invalid password"""
 
 
 def authenticate(func):
     """Authenticate for a method"""
-    def _authenticate(*args, user_name=None, session_token=None, **kwargs):
-        """Refresh a session for a user"""
-        if user_name is None or session_token is None:
-            raise NotAuthenticated()
-        user = User.by_hashed_name(user_name)
-        if user is None:
+    def login(*args, user_name=None, user_pass=None, **kwargs):
+        """Login a user"""
+        if user_name is None:
             raise NoSuchUser()
-        elif user.locked:
-            raise UserLocked()
+        elif user_pass is None:
+            raise NoPassword()
         else:
-            for session in Session.select().limit(1).where(Session.user
-                                                           == user):
-                if session.valid:
-                    if session.token == session_token:
-                        session = session.refresh()
-                        result = func(*args, **kwargs)
-                        result.session_token = session.token
-                        return result
+            user = User.by_hashed_name(user_name)
+            if user is None:
+                raise NoSuchUser()
+            elif user.locked:
+                raise UserLocked()
+            elif user.passwd == user_pass:
+                for session in Session.select().limit(1).where(Session.user
+                                                               == user):
+                    if session.valid:
+                        raise SessionExists()
                     else:
-                        raise InvalidCredentials()
+                        session.terminate()
                 else:
-                    session.terminate()
-                    raise SessionTimeout()
+                    session = Session.start(user)
+                    user.failed_logins = 0
+                    user.save()
+                    result = func(*args, **kwargs)
+                    result.session_token = session.token
+                    return result
             else:
-                raise NotLoggedIn()
+                user.failed_logins += 1
+                user.save()
+                raise InvalidCredentials()
 
-    return _authenticate
+    def session(*args, user_name=None, session_token=None, **kwargs):
+        """Refresh a session for a user"""
+        if user_name is None:
+            raise NoSuchUser()
+        elif session_token is None:
+            raise NoSessionToken()
+        else:
+            user = User.by_hashed_name(user_name)
+            if user is None:
+                raise NoSuchUser()
+            elif user.locked:
+                raise UserLocked()
+            else:
+                for session in Session.select().limit(1).where(Session.user
+                                                               == user):
+                    if session.valid:
+                        if session.token == session_token:
+                            session = session.refresh()
+                            result = func(*args, **kwargs)
+                            result.session_token = session.token
+                            return result
+                        else:
+                            raise InvalidCredentials()
+                    else:
+                        session.terminate()
+                        raise SessionTimeout()
+                else:
+                    raise NotLoggedIn()
+
+    def authenticate(*args, user_name=None, session_token=None,
+                     user_pass=None, **kwargs):
+        """Authenticate the user via active session or else password"""
+        if user_name is None:
+            raise InvalidCredentials()
+        else:
+            if session_token is None:
+                if user_pass is None:
+                    raise InvalidCredentials()
+                else:
+                    return login(*args, user_name=user_name,
+                                 user_pass=user_pass, **kwargs)
+            else:
+                return session(*args, user_name=user_name,
+                               session_token=session_token, **kwargs)
+
+    return authenticate
 
 
 def authorize(func):
     """Adds authorization to a method"""
-    def _authorize_group(group, service):
+    def authorize_group(group, service):
         """Determines whether a user is allowed to access a certain resource"""
         for _ in (GroupService.select()
                   .where(GroupService.group == group
@@ -80,7 +115,7 @@ def authorize(func):
         else:
             raise UnauthorizedGroup()
 
-    def _authorize(*args, user_name=None, service_name=None, **kwargs):
+    def authorize(*args, user_name=None, service_name=None, **kwargs):
         """Determines whether a user is allowed to access a certain resource"""
         if user_name is None or service_name is None:
             raise NotAuthorized()
@@ -93,7 +128,7 @@ def authorize(func):
             service = Service.by_name(service_name)
             if service is None:
                 raise NoSuchService()
-            elif _authorize_group(user.group, service):
+            elif authorize_group(user.group, service):
                 for _ in (UserService.select()
                           .where(UserService.user == user
                                  and UserService.service == service)):
@@ -103,4 +138,4 @@ def authorize(func):
             else:
                 raise UnauthorizedGroup()
 
-    return _authorize
+    return authorize

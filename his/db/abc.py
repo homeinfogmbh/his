2934e445f25a3bb1f0f515a746f0317d4850e67f
Richard Neumann <r.neumann@homeinfo.de>
2015-01-12 15:10:03 +0100

    Moved to HIModel

--- his/db/abc.py
+++ his/db/abc.py
@@ -2,32 +2,19 @@
 Basic HIS database definitions
 """
 from ..config import db
-from peewee import Model, MySQLDatabase
+from homeinfo.db import HIModel
+from peewee import MySQLDatabase
 
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
 __date__ = '09.10.2014'
 __all__ = ['HISModel']
 
 
-class HISModel(Model):
-    """
-    Generic HOMEINFO Integrated Service database model
-    """
+class HISModel(HIModel):
+    """Generic HOMEINFO Integrated Service database model"""
     class Meta:
         database = MySQLDatabase(db.get('db'),
                                  host=db.get('HOST'),
                                  user=db.get('USER'),
                                  passwd=db.get('PASSWD'),
                                  threadlocals=True)
-
-    def __enter__(self):
-        """Open a connection explicitly"""
-        self._meta.database.connect()
-        return self
-
-    def __exit__(self, tpe, value, tb):
-        """Closes a connection if existent"""
-        try:
-            self._meta.database.close()
-        except:
-            pass

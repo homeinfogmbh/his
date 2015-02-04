8857ad2b4279ddb6f4a4a5f6185520c9cd5adf3e
Richard Neumann <mail@richard-neumann.de>
2015-02-04 21:59:32 +0100

    fix

--- his/db/abc.py
+++ his/db/abc.py
@@ -2,15 +2,16 @@
 Basic HIS database definitions
 """
 from ..config import db
-from homeinfo.db import HIModel
-from peewee import MySQLDatabase
+from peewee import Model, MySQLDatabase
+from homeinfo.db import improved
 
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
 __date__ = '09.10.2014'
 __all__ = ['HISModel']
 
 
-class HISModel(HIModel):
+@improved
+class HISModel(Model):
     """Generic HOMEINFO Integrated Service database model"""
     class Meta:
         database = MySQLDatabase(db.get('db'),

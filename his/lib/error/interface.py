aed29bdd6399cf6dc3fb903f418f0bba2a178c6f
Richard Neumann <r.neumann@homeinfo.de>
2015-07-14 12:27:44 +0200

    Fixed Errors

--- his/lib/error/interface.py
+++ his/lib/error/interface.py
@@ -5,7 +5,7 @@ from pcp import pcp
 __all__ = ['InterfaceError']
 
 
-# TODO: Let HTTP errors be handles by homeinfolib.wsgi.Error
+# TODO: Let HTTP errors be handles by homeinfo.lib.wsgi.Error
 
 class InterfaceError(Exception):
     """An error that can be rendered as a PCP exception"""
@@ -16,48 +16,6 @@ class InterfaceError(Exception):
         self._msg = msg
         self._desc = desc
         self._http_status = http_status or 500
-
-    @property
-    def code(self):
-        """Returns the error code"""
-        return self._code
-
-    @property
-    def msg(self):
-        """Returns the error message"""
-        return self._msg
-
-    @property
-    def desc(self):
-        """Returns the error description dictionary"""
-        return self._desc
-
-    @property
-    def http_status(self):
-        """Returns the HTTP status code"""
-        return self._http_status
-
-    def __dom__(self):
-        """Converts the message to a DOM model"""
-        rsp = pcp.pcp()
-        msg = pcp.Message()
-        msg.code = self.code
-        msg.msg = self.msg
-        for lang, text in self.desc.items():
-            t = pcp.MessageText(text)
-            t.lang = lang
-            msg.text.append(t)
-        rsp.msg = msg
-        rsp.signal = pcp.Signal.NACK    # @UndefinedVariable
-        return rsp
-
-    def __xml__(self):
-        """Renders itself as a PCP message to XML bytes"""
-        return self.__dom__().toxml(encoding='utf-8')
-
-    def __json__(self):
-        """Renders itself as a PCP message to JSON bytes"""
-        return self.__dom__().tojson(encoding='utf-8')
 
     def __int__(self):
         """Returns the error code number"""
@@ -87,6 +45,51 @@ class InterfaceError(Exception):
         """Greater-than comparison"""
         return self.__lt__(other) or self.__eq__(other)
 
+    @property
+    def code(self):
+        """Returns the error code"""
+        return self._code
+
+    @property
+    def msg(self):
+        """Returns the error message"""
+        return self._msg
+
+    @property
+    def desc(self):
+        """Returns the error description dictionary"""
+        return self._desc
+
+    @property
+    def http_status(self):
+        """Returns the HTTP status code"""
+        return self._http_status
+
+    @property
+    def dom(self):
+        """Converts the message to a DOM model"""
+        rsp = pcp.pcp()
+        msg = pcp.Message()
+        msg.code = self.code
+        msg.msg = self.msg
+        for lang, text in self.desc.items():
+            t = pcp.MessageText(text)
+            t.lang = lang
+            msg.text.append(t)
+        rsp.msg = msg
+        rsp.signal = pcp.Signal.NACK    # @UndefinedVariable
+        return rsp
+
+    @property
+    def xml(self):
+        """Renders itself as a PCP message to XML bytes"""
+        return self.dom.toxml(encoding='utf-8')
+
+    @property
+    def json(self):
+        """Renders itself as a PCP message to JSON bytes"""
+        return self.dom.tojson(encoding='utf-8')
+
 
 class InvalidCredentials(InterfaceError):
     """Indicates that a user tried to
@@ -94,20 +97,22 @@ class InvalidCredentials(InterfaceError):
     """
 
     def __init__(self):
-        super().__init__(1, 'INVALID_CREDENTIALS',
-                         {'EN': 'User name and / or password are invalid.',
-                          'DE': 'Benutzername und / oder Passwort ungültig.'},
-                         http_status=400)
+        super().__init__(
+            1, 'INVALID_CREDENTIALS',
+            {'EN': 'User name and / or password are invalid.',
+             'DE': 'Benutzername und / oder Passwort ungültig.'},
+            http_status=400)
 
 
 class SessionTimeout(InterfaceError):
     """Indicates that a user's session has times out"""
 
     def __init__(self):
-        super().__init__(2, 'SESSION_TIMED_OUT',
-                         {'EN': 'Session timed out.',
-                          'DE': 'Sitzung abgelaufen.'},
-                         http_status=403)
+        super().__init__(
+            2, 'SESSION_TIMED_OUT',
+            {'EN': 'Session timed out.',
+             'DE': 'Sitzung abgelaufen.'},
+            http_status=403)
 
 
 class SessionExists(InterfaceError):
@@ -116,40 +121,44 @@ class SessionExists(InterfaceError):
     """
 
     def __init__(self):
-        super().__init__(3, 'SESSION_EXISTS',
-                         {'EN': 'You are already logged in.',
-                          'DE': 'Sie sind bereits angemeldet.'},
-                         http_status=400)
+        super().__init__(
+            3, 'SESSION_EXISTS',
+            {'EN': 'You are already logged in.',
+             'DE': 'Sie sind bereits angemeldet.'},
+            http_status=400)
 
 
 class UserLocked(InterfaceError):
     """Indicates that a user has been locked"""
 
     def __init__(self):
-        super().__init__(4, 'USER_LOCKED',
-                         {'EN': 'User account locked.',
-                          'DE': 'Benutzerkonto gesperrt.'},
-                         http_status=403)
+        super().__init__(
+            4, 'USER_LOCKED',
+            {'EN': 'User account locked.',
+             'DE': 'Benutzerkonto gesperrt.'},
+            http_status=403)
 
 
 class NotLoggedIn(InterfaceError):
     """Indicates that a user has no active session"""
 
     def __init__(self):
-        super().__init__(5, 'NOT_LOGGED_IN',
-                         {'EN': 'You are not logged in.',
-                          'DE': 'Sie sind nicht angemeldet.'},
-                         http_status=403)
+        super().__init__(
+            5, 'NOT_LOGGED_IN',
+            {'EN': 'You are not logged in.',
+             'DE': 'Sie sind nicht angemeldet.'},
+            http_status=403)
 
 
 class NoSuchService(InterfaceError):
     """Indicates that a service does not exist"""
 
     def __init__(self):
-        super().__init__(6, 'NO_SUCH_SERVICE',
-                         {'EN': 'The requested service does not exist.',
-                          'DE': 'Der angeforderte Dienst existiert nicht.'},
-                         http_status=404)
+        super().__init__(
+            6, 'NO_SUCH_SERVICE',
+            {'EN': 'The requested service does not exist.',
+             'DE': 'Der angeforderte Dienst existiert nicht.'},
+            http_status=404)
 
 
 class UnauthorizedUser(InterfaceError):
@@ -158,12 +167,13 @@ class UnauthorizedUser(InterfaceError):
     """
 
     def __init__(self):
-        super().__init__(7, 'UNAUTHORIZED_USER',
-                         {'EN': 'You are not allowed to access'
-                          ' the requested resource.',
-                          'DE': 'Sie sind nicht berechtigt, auf die'
-                          ' angeforderte Ressource zuzugreifen.'},
-                         http_status=401)
+        super().__init__(
+            7, 'UNAUTHORIZED_USER',
+            {'EN': 'You are not allowed to access'
+             ' the requested resource.',
+             'DE': 'Sie sind nicht berechtigt, auf die'
+             ' angeforderte Ressource zuzugreifen.'},
+            http_status=401)
 
 
 class UnauthorizedGroup(InterfaceError):
@@ -172,12 +182,13 @@ class UnauthorizedGroup(InterfaceError):
     """
 
     def __init__(self):
-        super().__init__(8, 'UNAUTHORIZED_GROUP',
-                         {'EN': 'Your group are not allowed to'
-                          'access the requested resource.',
-                          'DE': 'Ihre Gruppe ist nicht berechtigt, auf die'
-                          'angeforderte Ressource zuzugreifen.'},
-                         http_status=401)
+        super().__init__(
+            8, 'UNAUTHORIZED_GROUP',
+            {'EN': 'Your group are not allowed to'
+             'access the requested resource.',
+             'DE': 'Ihre Gruppe ist nicht berechtigt, auf die'
+             ' angeforderte Ressource zuzugreifen.'},
+            http_status=401)
 
 
 class NotAuthenticated(InterfaceError):
@@ -186,10 +197,11 @@ class NotAuthenticated(InterfaceError):
     """
 
     def __init__(self):
-        super().__init__(9, 'NOT_AUTHENTICATED',
-                         {'EN': 'Go away!',
-                          'DE': 'Geh weg!'},
-                         http_status=403)
+        super().__init__(
+            9, 'NOT_AUTHENTICATED',
+            {'EN': 'Go away!',
+             'DE': 'Geh weg!'},
+            http_status=403)
 
 
 class NotAuthorized(InterfaceError):
@@ -198,10 +210,11 @@ class NotAuthorized(InterfaceError):
     """
 
     def __init__(self):
-        super().__init__(10, 'NOT_AUTHORIZED',
-                         {'EN': 'Go away!',
-                          'DE': 'Geh weg!'},
-                         http_status=401)
+        super().__init__(
+            10, 'NOT_AUTHORIZED',
+            {'EN': 'Go away!',
+             'DE': 'Geh weg!'},
+            http_status=401)
 
 
 class InternalServerError(InterfaceError):
@@ -210,10 +223,11 @@ class InternalServerError(InterfaceError):
     """
 
     def __init__(self):
-        super().__init__(11, 'INTERNAL_SERVER_ERROR',
-                         {'EN': 'Internal server error.',
-                          'DE': 'Interner Server-Fehler.'},
-                         http_status=500)
+        super().__init__(
+            11, 'INTERNAL_SERVER_ERROR',
+            {'EN': 'Internal server error.',
+             'DE': 'Interner Server-Fehler.'},
+            http_status=500)
 
 
 class NoSuchResource(InterfaceError):
@@ -222,10 +236,11 @@ class NoSuchResource(InterfaceError):
     """
 
     def __init__(self):
-        super().__init__(12, 'NO_SUCH_RESOURCE',
-                         {'EN': 'No such resource.',
-                          'DE': 'Keine solche Ressource.'},
-                         http_status=404)
+        super().__init__(
+            12, 'NO_SUCH_RESOURCE',
+            {'EN': 'No such resource.',
+             'DE': 'Keine solche Ressource.'},
+            http_status=404)
 
 
 class UnsupportedHTTPAction(InterfaceError):
@@ -235,7 +250,8 @@ class UnsupportedHTTPAction(InterfaceError):
     """
 
     def __init__(self):
-        super().__init__(13, 'UNSUPPORTED_HTTP_ACTION',
-                         {'EN': 'Unsupported HTTP action.',
-                          'DE': 'Nicht-unterstützte HTTP-Aktion.'},
-                         http_status=405)
+        super().__init__(
+            13, 'UNSUPPORTED_HTTP_ACTION',
+            {'EN': 'Unsupported HTTP action.',
+             'DE': 'Nicht-unterstützte HTTP-Aktion.'},
+            http_status=405)

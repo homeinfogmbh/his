dfd47cd8536ec324853abd5c6241c8c0efc66b9d
Richard Neumann <r.neumann@homeinfo.de>
2015-07-28 15:49:21 +0200

    Made crypto stuff OO

--- his/lib/crypto.py
+++ his/lib/crypto.py
@@ -3,33 +3,47 @@
 import hmac
 from uuid import uuid4
 from hashlib import sha256
+from his.config import his_config
 
-__all__ = ['hashpw', 'chkpw']
-
-
-# XXX: Avoid to change!
-_PEPPER = 'a852eca7-7a23-4b72-a48d-997047e18ef0'
-_ITERS = 10000
+__all__ = ['Crypto']
 
 
-def _hash_pw(passwd, salt):
-    """Hashes a password"""
-    psp = salt + _PEPPER + passwd
-    return sha256(psp.encode()).hexdigest()
+class Crypto():
+    """Crypto manager"""
 
+    def __init__(self, cipher=None):
+        """Sets the hashtype"""
+        if cipher is None:
+            self.cipher = sha256
+        else:
+            self.cipher = cipher
 
-def hashpw(plaintext):
-    """Hashes a password"""
-    salt = hmac.new(str(uuid4()).encode()).hexdigest()
-    hashed_pw = _hash_pw(plaintext, salt)
-    for _ in range(0, _ITERS):
-        hashed_pw = _hash_pw(hashed_pw, salt)
-    return (hashed_pw, salt)
+    def _hash_pw(self, passwd, salt):
+        """Hashes a password"""
+        psp = salt + self.pepper + passwd
+        return self.cipher(psp.encode()).hexdigest()
 
+    @property
+    def pepper(self):
+        """Returns the pepper string"""
+        return hmac.new(his_config.crypto['PEPPER'].encode()).hexdigest()
 
-def chkpw(plaintext, pwhash, salt):
-    """Verifies a plain text password"""
-    hashed_pw = _hash_pw(plaintext, salt)
-    for _ in range(0, _ITERS):
-        hashed_pw = _hash_pw(hashed_pw, salt)
-    return True if hashed_pw == pwhash else False
+    @property
+    def iters(self):
+        """Returns the pepper string"""
+        return int(his_config.crypto['ITERS'])
+
+    def hashpw(self, plaintext):
+        """Hashes a password"""
+        salt = hmac.new(str(uuid4()).encode()).hexdigest()
+        hashed_pw = self._hash_pw(plaintext, salt)
+        for _ in range(0, self.iters):
+            hashed_pw = self._hash_pw(hashed_pw, salt)
+        return (hashed_pw, salt)
+
+    def chkpw(self, plaintext, pwhash, salt):
+        """Verifies a plain text password"""
+        hashed_pw = self._hash_pw(plaintext, salt)
+        for _ in range(0, self.iters):
+            hashed_pw = self._hash_pw(hashed_pw, salt)
+        return True if hashed_pw == pwhash else False

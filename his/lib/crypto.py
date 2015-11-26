f40d69ab4e0437db6ec4dc5529e06e7d77861196
Richard Neumann <r.neumann@homeinfo.de>
2015-11-26 12:16:06 +0100

    Fixed crypto lib

--- his/lib/crypto.py
+++ his/lib/crypto.py
@@ -1,13 +1,19 @@
 """HIS cryptographic library"""
 
 import hmac
-from uuid import uuid4
 from hashlib import sha256
 from itertools import repeat
+from string import printable
+from random import choice
 
 from ..config import his_config
 
-__all__ = ['PasswordManager']
+__all__ = ['PasswordGenerationError', 'PasswordManager']
+
+
+class PasswordGenerationError(Exception):
+    """Indicates an error during password generation"""
+    pass
 
 
 class PasswordManager():
@@ -15,19 +21,23 @@ class PasswordManager():
 
     def _hash_pw(self, passwd, salt):
         """Hashes a password"""
-        psp = passwd + salt + self._pepper
-        return sha256(psp.encode()).hexdigest()
+        pw_salt = passwd + salt
+        pw_salt_hmac = hmac.new(pw_salt.encode()).hexdigest()
+        pw_salt_hmac_pepper = pw_salt_hmac + self._pepper
+        pwhash = sha256(pw_salt_hmac_pepper.encode()).hexdigest()
+        return pwhash
 
     def _iter_hash(self, passwd, salt):
         """Hashes the password <self.iters> times"""
-        hashed_pw = self._hash_pw(passwd, salt)
-        for _ in repeat(None, self._iters):
-            hashed_pw = self._hash_pw(hashed_pw, salt)
-        return hashed_pw
+        pwhash = self._hash_pw(passwd, salt)
+        # Loop hashes, compensating for one already performed hash
+        for _ in repeat(None, self._iters - 1):
+            pwhash = self._hash_pw(pwhash, salt)
+        return pwhash
 
     @property
     def _iters(self):
-        """Returns the pepper string"""
+        """Returns the amount of hashing iterations"""
         iters = int(his_config.crypto['ITERS'])
         if iters > 0:
             return iters
@@ -42,11 +52,25 @@ class PasswordManager():
 
     def hash(self, plaintext):
         """Hashes a password"""
-        salt = hmac.new(str(uuid4()).encode()).hexdigest()
-        hashed_pw = self._iter_hash(plaintext, salt)
-        return (hashed_pw, salt)
+        salt = self.random()
+        pwhash = self._iter_hash(plaintext, salt)
+        return (pwhash, salt)
 
     def verify(self, plaintext, pwhash, salt):
         """Verifies a plain text password"""
         hashed_pw = self._iter_hash(plaintext, salt)
-        return True if hashed_pw == pwhash else False
+        return hashed_pw == pwhash
+
+    def random(self, length=16):
+        """Generates a random password"""
+        password = ''
+        for _ in repeat(None, length):
+            password += choice(printable)
+        pwlen = len(password)
+        if pwlen == length:
+            return password
+        else:
+            raise PasswordGenerationError(
+                'Expected password of length {0} but got '
+                'password of length {1}'.format(length, pwlen)
+            )

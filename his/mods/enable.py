6ef938847e7bfd2d5242684c870d4f941972bd99
Richard Neumann <r.neumann@homeinfo.de>
2016-06-15 13:36:56 +0200

    fix

--- his/mods/enable.py
+++ his/mods/enable.py
@@ -18,8 +18,11 @@ class Service(HISService):
         """Logs in the user"""
         try:
             session_token = self.query_dict['token']
+            session = Session.get(Session.token == session_token)
         except KeyError:
             return Error('No session specified.', status=400)
+        except DoesNotExist:
+            return Error('No such session.', status=400)
         else:
             try:
                 service_name = self.query_dict['service']
@@ -29,49 +32,67 @@ class Service(HISService):
             except DoesNotExist:
                 return Error('No such service.', status=400)
             else:
-                customer_id = self.query_dict.get('customer')
-                account_name = self.query_dict.get('account')
+                if session.active:
+                    account = session.account
+                    customer_id = self.query_dict.get('customer')
+                    account_name = self.query_dict.get('account')
 
-                if customer_id is None and account_name is None:
-                    return Error('No customer or accout specified.',
-                                 status=400)
+                    if customer_id is None and account_name is None:
+                        return Error('No customer or accout specified.',
+                                     status=400)
 
-                if customer_id is not None:
-                    try:
-                        cid = int(customer_id)
-                    except ValueError:
-                        return Error('Invalid customer ID.', status=400)
-                    else:
-                        try:
-                            customer = Customer.get(Customer.id == cid)
-                        except DoesNotExist:
-                            return Error('No such customer.', status=400)
-                        else:
+                    if customer_id is not None:
+                        if account.root:
                             try:
-                                CustomerService.get(
-                                    (CustomerService.customer == customer) &
-                                    (CustomerService.service == service))
-                            except DoesNotExist:
-                                customer_service = CustomerService()
-                                customer_service.customer = customer
-                                customer_service.service = service
-
-                                if customer_service.save():
-                                    return OK('Service added for customer.')
-                                else:
-                                    Error('Could not add service.', status=500)
+                                cid = int(customer_id)
+                            except ValueError:
+                                return Error('Invalid customer ID.',
+                                             status=400)
                             else:
-                                Error('Service already enabled.', status=400)
+                                try:
+                                    customer = Customer.get(Customer.id == cid)
+                                except DoesNotExist:
+                                    return Error('No such customer.',
+                                                 status=400)
+                                else:
+                                    try:
+                                        CustomerService.get(
+                                            (CustomerService.customer ==
+                                                customer) &
+                                            (CustomerService.service ==
+                                                service))
+                                    except DoesNotExist:
+                                        customer_service = CustomerService()
+                                        customer_service.customer = customer
+                                        customer_service.service = service
 
-                if account_name is not None:
-                    try:
-                        account = Account.get(Account.name == account_name)
-                    except DoesNotExist:
-                        return Error('No such account.', status=400)
-                    else:
-                        try:
-                            account.services.add(service)
-                        except InconsistencyError as e:
-                            return Error(e.msg, status=400)
+                                        if customer_service.save():
+                                            return OK(
+                                                'Service added for customer.')
+                                        else:
+                                            Error('Could not add service.',
+                                                  status=500)
+                                    else:
+                                        Error('Service already enabled.',
+                                              status=400)
                         else:
-                            return OK('Service added for account.')
+                            Error('You are not a root user.', status=400)
+
+                    if account_name is not None:
+                        if account.admin:
+                            try:
+                                account = Account.get(
+                                    Account.name == account_name)
+                            except DoesNotExist:
+                                return Error('No such account.', status=400)
+                            else:
+                                try:
+                                    account.services.add(service)
+                                except InconsistencyError as e:
+                                    return Error(e.msg, status=400)
+                                else:
+                                    return OK('Service added for account.')
+                        else:
+                            return Error('You are not an admin.', status=400)
+                else:
+                    return Error('Not logged in.', status=400)

9506a0027a1d5a8a7e8070e297624b6b1aa73cf5
Richard Neumann <r.neumann@homeinfo.de>
2016-06-16 15:20:38 +0200

    Prototyping immobit

--- his/api.py
+++ his/api.py
@@ -2,17 +2,76 @@
 
 from peewee import DoesNotExist
 
-from homeinfo.lib.wsgi import RequestHandler
+from homeinfo.lib.wsgi import Error, RequestHandler
 
 from his.orm import Service
 
-__all__ = ['IncompleteImplementationError', 'HISService']
+__all__ = [
+    'IncompleteImplementationError',
+    'HISAPIError',
+    'SessionError',
+    'NoSessionSpecified',
+    'NoSuchSession',
+    'SessionExpired',
+    'HISService',
+    'SessionService',
+    'AccountService',
+    'CheckedAccountService']
 
 
 class IncompleteImplementationError(NotImplementedError):
     """Indicates an incomplete implementation of the service"""
 
     pass
+
+
+class HISAPIError(Error):
+    """Indicates errors for the WebAPI"""
+
+    pass
+
+
+class SessionError(HISAPIError):
+    """Indicates errors with sessions"""
+
+    pass
+
+
+class NoSessionSpecified(SessionError):
+    """Indicates a missing session"""
+
+    def __init__(self):
+        super().__init__('No session specified.', status=400)
+
+
+class NoSuchSession(SessionError):
+    """Indicates that the specified session does not exist"""
+
+    def __init__(self):
+        super().__init__('No such session.', status=400)
+
+
+class SessionExpired(SessionError):
+    """Indicates that the specified session has expired"""
+
+    def __init__(self):
+        super().__init__('Session expired.', status=400)
+
+
+class ServiceNotRegistered(HISAPIError):
+    """Indicates that the service is not registered"""
+
+    def __init__(self):
+        super().__init__('Service is not registered.', status=500)
+
+
+class NotAuthorized(HISAPIError):
+    """Indicates that the respective entity
+    is not authorized to use the service
+    """
+
+    def __init__(self):
+        super().__init__('Not authorized.', status=400)
 
 
 class HISService(RequestHandler):
@@ -25,7 +84,9 @@ class HISService(RequestHandler):
 
     @classmethod
     def install(cls):
-        """Installs the service into the registered database"""
+        """Installs the service into
+        the registered database
+        """
         if cls.PATH is None or cls.NAME is None:
             raise IncompleteImplementationError()
         else:
@@ -50,3 +111,67 @@ class HISService(RequestHandler):
                             return True
 
                 return False
+
+
+class SessionService(HISService):
+    """A HIS service that is session-aware"""
+
+    @property
+    def session(self):
+        """Returns the session or raises an error"""
+        try:
+            session_token = self.query_dict['session']
+        except KeyError:
+            raise NoSessionSpecified()
+        else:
+            try:
+                session = Session.get(Session.token == session_token)
+            except DoesNotExist:
+                raise NoSuchSession()
+            else:
+                if session.active:
+                    return session
+                else:
+                    raise SessionExpired()
+
+
+class AccountService(SessionService):
+    """A HIS service that is account- and customer-aware"""
+
+    @property
+    def account(self):
+        """Returns the respective account"""
+        return self.session.account
+
+    @property
+    def customer(self):
+        """Returns the respective customer"""
+        return self.session.account.customer
+
+
+class CheckedAccountService(AccountService):
+    """A HIS service that checks whether
+    the account is enabled for it
+    """
+
+    def __call__(self):
+        """Determines whether the account
+        is allowed to use this service
+        """
+        try:
+            path = self.__class__.PATH
+        except AttributeError:
+            raise IncompleteImplementationError()
+        else:
+            if not path:
+                raise IncompleteImplementationError()
+            else:
+                try:
+                    service = Service.get(Service.path == path)
+                except DoesNotExist:
+                    raise ServiceNotRegistered()
+                else:
+                    if service in self.account.services:
+                        return super().__call__()
+                    else:
+                        raise NotAuthorized()
---
+++ his/mods/immobit.py
@@ -0,0 +1,50 @@
+"""A real estate data manipulation library"""
+
+from peewee import DoesNotExist
+
+from homeinfo.crm import Customer
+from homeinfo.lib.wsgi import Error, OK, JSON
+
+from openimmo.factories import openimmo, uebertragung, anbieter
+from openimmo.openimmo import Umfang
+
+from openimmodb3.db import Immobilie
+
+from his.api import CheckedAccountService
+from his.crypto import load
+from his.orm import InconsistencyError, AlreadyLoggedIn, Service, \
+    CustomerService, Account, Session
+
+
+class ImmobitHandler(CheckedAccountService):
+    """Handles requests for ImmoBit"""
+
+    PATH = 'immobit'
+    NAME = 'ImmoBit'
+    DESCRIPTION = 'Immobiliendatenverwaltung'
+    PROMOTE = True
+
+    @property
+    def filters(self):
+        """Returns filters"""
+        try:
+            filter_str = self.query_dict['filter']
+        except KeyError:
+            filters = []
+        else:
+            filters = [f for f in filter_str.split(',') if f]
+
+    def get(self):
+        """Handles GET requests"""
+        # TODO: Handle filters
+        uebertragung_dom = uebertragung(
+            self.__class__.NAME,
+            'latest',
+            'info@homeinfo.de',
+            umfang=Umfang.VOLL)
+        anbieter_dom = anbieter(repr(self.customer), str(self.customer))
+
+        for immobilie in Immobilie.by_cid(self.customer):
+            anbieter_dom.immobilie.append(immobilie.dom)
+
+        return XML(anbieter_dom)

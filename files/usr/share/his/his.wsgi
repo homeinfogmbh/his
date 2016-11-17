6742291739ac82859f20eb9c60eb0912fc930c59
Richard Neumann <r.neumann@homeinfo.de>
2016-11-17 15:16:43 +0100

    Added SessionCleaner()

--- files/usr/share/his/his.wsgi
+++ files/usr/share/his/his.wsgi
@@ -2,6 +2,8 @@
 """HIS core handler"""
 
 from homeinfo.lib.rest import RestApp
-from his.core import HISProxy
+from his.core import HISProxy, SessionCleaner
 
+sc = SessionCleaner()
+sc.start()
 application = RestApp(HISProxy('his'), cors=True, debug=True)
--- his/core.py
+++ his/core.py
@@ -1,13 +1,17 @@
 """Core services"""
 
+from itertools import repeat
+from time import sleep
+from threading import Thread
+
 from peewee import DoesNotExist
 
 from homeinfo.lib.log import LoggingClass
 from homeinfo.lib.wsgi import Error
 
-from his.orm import Service
+from his.orm import Service, Session
 
-__all__ = ['HISProxy']
+__all__ = ['HISProxy', 'SessionCleaner']
 
 
 class HandlerNotAvailable(Error):
@@ -41,3 +45,51 @@ class HISProxy(LoggingClass):
                 handler = service.handler
                 self.logger.info('Loading handler "{}"'.format(handler))
                 return handler
+
+
+class SessionCleaner():
+    """Periodically cleans up the sessions"""
+
+    def __init__(self, interval=60):
+        """Sets the cleanup interval"""
+        self.interval = interval
+        self._running = False
+        self._thread = None
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *_):
+        self.stop()
+
+    def _run(self):
+        """The actual cleanup loop"""
+        while self._running:
+            Session.cleanup()
+
+            for _ in repeat(None, self.interval):
+                if self._running:
+                    sleep(1)
+                else:
+                    break
+
+    def start(self):
+        """Starts the cleanup thread"""
+        if self._thread is None:
+            self._running = True
+            self._thread = Thread(target=self._run)
+            self._thread.start()
+            return True
+        else:
+            return False
+
+    def stop(self):
+        """Stops the cleanup thread"""
+        self._running = False
+
+        if self._thread is not None:
+            self._thread.join()
+            self._thread = None
+
+        return True

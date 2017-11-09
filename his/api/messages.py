fb0b6ee7a9952cc4c1949f7f22bf42dfdb0a4bf0
Richard Neumann <r.neumann@homeinfo.de>
2017-11-09 12:22:11 +0100

    Refactored docstrings.

--- his/api/messages.py
+++ his/api/messages.py
@@ -1,4 +1,4 @@
-"""Web API errors"""
+"""Web API errors."""
 
 from configparser import ConfigParser
 from wsgilib import JSON
@@ -51,7 +51,7 @@ __all__ = [
 
 
 def locales(locales_file):
-    """Sets the locales file"""
+    """Decorator to set the locales file."""
 
     parser = ConfigParser()
     parser.read(locales_file)
@@ -65,12 +65,12 @@ def locales(locales_file):
 
 @locales('/etc/his.d/locale/core.ini')
 class HISMessage(JSON):
-    """Indicates errors for the WebAPI"""
+    """Indicates errors for the WebAPI."""
 
     STATUS = 200
 
     def __init__(self, *data, cors=None, lang='de_DE', **fields):
-        """Initializes the message"""
+        """Initializes the message."""
         dictionary = {'message': self.message(lang, data=data)}
         dictionary.update(fields)
         super().__init__(dictionary, status=self.STATUS, cors=cors)
@@ -78,222 +78,222 @@ class HISMessage(JSON):
     @classproperty
     @classmethod
     def locales(cls):
-        """Returns the classes locales"""
+        """Returns the classes locales."""
         try:
             return cls._parser[cls.__name__]
         except KeyError:
             return {}
 
     def message(self, lang, data=None):
-        """Returns the respective message"""
+        """Returns the respective message."""
         try:
             message = self.__class__.locales[lang]
         except KeyError:
             return 'Could not get locale: {}.'.format(lang)
-        else:
-            if data:
-                message.format(*data)
 
-            return message
+        if data:
+            message.format(*data)
+
+        return message
 
 
 class HISServerError(HISMessage):
-    """Indicates errors for the WebAPI"""
+    """Indicates errors for the WebAPI."""
 
     STATUS = 500
 
 
 class IncompleteImplementationError(HISServerError):
-    """Indicates an incomplete implementation of the service"""
+    """Indicates an incomplete implementation of the service."""
 
     pass
 
 
 class ServiceError(HISServerError):
-    """General service error"""
+    """General service error."""
 
     pass
 
 
 class ServiceNotRegistered(ServiceError):
-    """Indicates that the service is not registered"""
+    """Indicates that the service is not registered."""
 
     pass
 
 
 class NoServiceSpecified(ServiceError):
-    """Indicates that no service was specified"""
+    """Indicates that no service was specified."""
 
     STATUS = 420
 
 
 class NoSuchService(ServiceError):
-    """Indicates that the service does not exist"""
+    """Indicates that the service does not exist."""
 
     STATUS = 404
 
 
 class HISDataError(HISMessage):
-    """Indicates errors in sent data"""
+    """Indicates errors in sent data."""
 
     STATUS = 422
 
 
 class NoDataProvided(HISDataError):
-    """Indicates missing data"""
+    """Indicates missing data."""
 
     pass
 
 
 class NotAnInteger(HISDataError):
-    """Indicates missing credentials"""
+    """Indicates missing credentials."""
 
     pass
 
 
 class InvalidData(HISDataError):
-    """Indicates missing data"""
+    """Indicates missing data."""
 
     pass
 
 
 class InvalidUTF8Data(InvalidData):
-    """Indicates that the data provided was not UTF-8 text"""
+    """Indicates that the data provided was not UTF-8 text."""
 
     pass
 
 
 class InvalidJSON(InvalidData):
-    """Indicates that the JSON data is invalid"""
+    """Indicates that the JSON data is invalid."""
 
     pass
 
 
 class InvalidCustomerID(InvalidData):
-    """Indicates that an invalid customer ID was specified"""
+    """Indicates that an invalid customer ID was specified."""
 
     pass
 
 
 class HISAPIError(HISMessage):
-    """Indicates errors for the WebAPI"""
+    """Indicates errors for the WebAPI."""
 
     STATUS = 400
 
 
 class NotAuthorized(HISAPIError):
     """Indicates that the respective entity
-    is not authorized to use the service
+    is not authorized to use the service.
     """
 
     STATUS = 403
 
 
 class LoginError(HISAPIError):
-    """Indicates login errors"""
+    """Indicates login errors."""
 
     STATUS = 401
 
 
 class MissingCredentials(LoginError):
-    """Indicates missing credentials"""
+    """Indicates missing credentials."""
 
     pass
 
 
 class InvalidCredentials(LoginError):
-    """Indicates invalid credentials"""
+    """Indicates invalid credentials."""
 
     pass
 
 
 class AccountLocked(LoginError):
-    """Indicates that the account is locked"""
+    """Indicates that the account is locked."""
 
     STATUS = 423
 
 
 class AlreadyLoggedIn(LoginError):
-    """Indicates that the account is already running a session"""
+    """Indicates that the account is already running a session."""
 
     STATUS = 409
 
 
 class SessionError(HISAPIError):
-    """Indicates errors with sessions"""
+    """Indicates errors with sessions."""
 
     pass
 
 
 class NoSessionSpecified(SessionError):
-    """Indicates a missing session"""
+    """Indicates a missing session."""
 
     STATUS = 420
 
 
 class NoSuchSession(SessionError):
-    """Indicates that the specified session does not exist"""
+    """Indicates that the specified session does not exist."""
 
     STATUS = 404
 
 
 class SessionExpired(SessionError):
-    """Indicates that the specified session has expired"""
+    """Indicates that the specified session has expired."""
 
     STATUS = 410
 
 
 class DurationOutOfBounds(SessionError):
     """Indicates that an out of bounds duration
-    was secified on session creation or renewal
+    was secified on session creation or renewal.
     """
 
     pass
 
 
 class CustomerError(HISAPIError):
-    """Base for errors regarding customers"""
+    """Base for errors regarding customers."""
 
     STATUS = 404
 
 
 class NoCustomerSpecified(CustomerError):
-    """Indicates that no customer was specified"""
+    """Indicates that no customer was specified."""
 
     STATUS = 420
 
 
 class NoSuchCustomer(CustomerError):
-    """Indicates that the customer does not exist"""
+    """Indicates that the customer does not exist."""
 
     STATUS = 404
 
 
 class AccountError(HISAPIError):
-    """Base for errors regarding accounts"""
+    """Base for errors regarding accounts."""
 
     STATUS = 404
 
 
 class NoAccountSpecified(AccountError):
-    """Indicates that no account has been specified"""
+    """Indicates that no account has been specified."""
 
     STATUS = 404
 
 
 class NoSuchAccount(AccountError):
-    """Indicates that an account with the specified name does not exist"""
+    """Indicates that an account with the specified name does not exist."""
 
     STATUS = 404
 
 
 class AccountPatched(AccountError):
-    """Indicates that the account has been patched successfully"""
+    """Indicates that the account has been patched successfully."""
 
     STATUS = 200
 
 
 class BoundaryError(HISAPIError):
-    """Base for boundry violation errors"""
+    """Base for boundry violation errors."""
 
     STATUS = 403

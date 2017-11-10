d01f8d311520462dd44a85e1e22ba957ddb30eca
Richard Neumann <r.neumann@homeinfo.de>
2017-11-10 12:55:34 +0100

    Refactored services.

--- his/mods/service.py
+++ his/mods/service.py
@@ -1,4 +1,4 @@
-"""HIS meta services"""
+"""HIS meta services."""
 
 from peewee import DoesNotExist
 
@@ -16,8 +16,9 @@ __all__ = [
     'INSTALL']
 
 
+# TODO: Outsource to his.api.messages.
 class AmbiguousTarget(JSON):
-    """Indicates that the selected target is ambiguous"""
+    """Indicates that the selected target is ambiguous."""
 
     STATUS = 400
     LOCALE = {
@@ -26,75 +27,83 @@ class AmbiguousTarget(JSON):
 
 
 class ServicePermissions(AuthenticatedService):
-    """Handles service permissions"""
+    """Handles service permissions."""
 
     NODE = 'services'
     NAME = 'services manager'
     DESCRIPTION = 'Manages services permissions'
     PROMOTE = False
 
-    def post(self):
-        """Allows services"""
-        session = self.session
+    def add_customer_service(self, customer_id, service):
+        """Allows the respective customer to use the given service."""
+        if not self.account.root:
+            raise Error('You are not a root user.', status=400) from None
 
+        try:
+            cid = int(customer_id)
+        except ValueError:
+            raise InvalidCustomerID() from None
+
+        try:
+            customer = Customer.get(Customer.id == cid)
+        except DoesNotExist:
+            raise NoSuchCustomer() from None
+
+        try:
+            CustomerService.get(
+                (CustomerService.customer == customer) &
+                (CustomerService.service == service))
+        except DoesNotExist:
+            customer_service = CustomerService()
+            customer_service.customer = customer
+            customer_service.service = service
+            customer_service.save()
+            return OK('Service added for customer.')
+        else:
+            return OK('Service already enabled.')
+
+    def add_account_service(self, account_name, service):
+        """Allows the respective account to use the given service."""
+        if not self.account.admin:
+            raise NotAuthorized()
+
+        try:
+            account = Account.get(
+                Account.name == account_name)
+        except DoesNotExist:
+            return Error('No such account.', status=400)
+
+        try:
+            account.services.add(service)
+        except InconsistencyError as error:
+            return Error(error.msg, status=400)
+        else:
+            return OK('Service added for account.')
+
+    def add_service(self, service):
+        """Sets permissions for the respective account."""
+        customer_id = self.query.get('customer')
+        account_name = self.query.get('account')
+
+        if customer_id is not None and account_name is not None:
+            return AmbiguousTarget()
+        elif customer_id is not None:
+            return self.add_customer_service(customer_id, service)
+        elif account_name is not None:
+            return self.add_account_service(account_name, service)
+
+        return Error('Neither customer, nor accout specified.', status=400)
+
+    def post(self):
+        """Allows services."""
         try:
             service = Service.get(Service.name == self.query['service'])
         except KeyError:
             raise NoServiceSpecified() from None
         except DoesNotExist:
             raise NoSuchService() from None
-        else:
-            account = session.account
-            customer_id = self.query.get('customer')
-            account_name = self.query.get('account')
 
-            if customer_id is not None and account_name is not None:
-                raise AmbiguousTarget() from None
-            elif customer_id is not None:
-                if account.root:
-                    try:
-                        cid = int(customer_id)
-                    except ValueError:
-                        raise InvalidCustomerID() from None
-
-                    try:
-                        customer = Customer.get(Customer.id == cid)
-                    except DoesNotExist:
-                        raise NoSuchCustomer() from None
-
-                    try:
-                        CustomerService.get(
-                            (CustomerService.customer == customer) &
-                            (CustomerService.service == service))
-                    except DoesNotExist:
-                        customer_service = CustomerService()
-                        customer_service.customer = customer
-                        customer_service.service = service
-                        customer_service.save()
-                        return OK('Service added for customer.')
-                    else:
-                        return OK('Service already enabled.')
-                else:
-                    raise Error('You are not a root user.', status=400)
-            elif account_name is not None:
-                if account.admin:
-                    try:
-                        account = Account.get(
-                            Account.name == account_name)
-                    except DoesNotExist:
-                        return Error('No such account.', status=400)
-
-                    try:
-                        account.services.add(service)
-                    except InconsistencyError as error:
-                        return Error(error.msg, status=400)
-                    else:
-                        return OK('Service added for account.')
-                else:
-                    raise NotAuthorized()
-            else:
-                return Error('No customer or accout specified.',
-                             status=400)
+        return self.add_service(service)
 
 
 INSTALL = [ServicePermissions]

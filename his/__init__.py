f3922a9a8ce30256506721b4499ae55af608dd02
Richard Neumann <r.neumann@homeinfo.de>
2017-12-08 11:18:36 +0100

    Updated API.

--- his/__init__.py
+++ his/__init__.py
@@ -1,15 +1,15 @@
 """HOMEINFO Integrated Services.
 
-
 The HOMEINFO Integrated Services (HIS) are a meta webservice
 on top of which actual web services with centralized authentication
 may be implemented.
 
 (C) 2017: HOMEINFO - Digitale Informationssysteme GmbH
 """
-from his.globals import SESSION, ACCOUNT, CUSTOMER, SU_ACCOUNT, SU_CUSTOMER
 from his.api import DATA, authenticated, authorized, admin, root
+from his.globals import SESSION, ACCOUNT, CUSTOMER, SU_ACCOUNT, SU_CUSTOMER
 from his.messages.common import locales, Message
+from his.orm import his_db, Account
 
 __all__ = [
     'SESSION',
@@ -23,4 +23,6 @@ __all__ = [
     'admin',
     'root',
     'locales',
-    'Message']
+    'Message',
+    'his_db',
+    'Account']

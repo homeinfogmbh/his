96178fdff038c9ad9f451bdc32ebed95b3fcec3d
Richard Neumann <r.neumann@homeinfo.de>
2018-01-05 10:18:39 +0100

    fix

--- his/wsgi/__init__.py
+++ his/wsgi/__init__.py
@@ -10,7 +10,5 @@ __all__ = ['APPLICATION']
 
 
 APPLICATION = Application('his', debug=True, cors=True)
-APPLICATION.set_endpoint('account', account.ROUTES)
-APPLICATION.set_endpoint('customer', customer.ROUTES)
-APPLICATION.set_endpoint('service', service.ROUTES)
-APPLICATION.set_endpoint('session', session.ROUTES)
+APPLICATION.add_routes(chain(
+    account.ROUTES, customer.ROUTES, service.ROUTES, session.ROUTES))

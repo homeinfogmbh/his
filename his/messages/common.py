ae91465267289198ace495fd109a44fc0aa891cd
Richard Neumann <r.neumann@homeinfo.de>
2018-01-24 16:33:10 +0100

    Introduced message metaclass.

--- his/messages/common.py
+++ his/messages/common.py
@@ -10,6 +10,24 @@ __all__ = ['locales', 'Message']
 DEFAULT_LANGUAGE = 'de_DE'
 
 
+class MessageNotFound(Exception):
+    """Indicates that the respective message could not be found."""
+
+    def __init__(self, message):
+        """Sets the respective message."""
+        super().__init__(message)
+        self.message = message
+
+
+class LanguageNotFound(Exception):
+    """Indicates that the respective language could not be found."""
+
+    def __init__(self, lang):
+        """Sets the respective language."""
+        super().__init__(lang)
+        self.lang = lang
+
+
 def locales(filename):
     """Decorator to set the locales file."""
 
@@ -18,34 +36,35 @@ def locales(filename):
     return parser
 
 
-class Message(JSON):
+class MetaMessage(type):
+    """Metaclass for messages."""
+
+    def __init__(cls, *args, **kwargs):
+        """Sets the class's respective locales."""
+        super().__init__(*args, **kwargs)
+
+        try:
+            cls.locales = cls.LOCALES[cls.__name__]
+        except KeyError:
+            raise MessageNotFound(cls.__name__)
+
+
+class Message(JSON, metaclass=MetaMessage):
     """Indicates errors for the WebAPI."""
 
-    STATUS = 200
     LOCALES = locales('/etc/his.d/locale/his.ini')
+    STATUS = 200
 
     def __init__(self, *data, lang=DEFAULT_LANGUAGE, **fields):
         """Initializes the message."""
-        dictionary = {'message': self.message(lang, data=data)}
-        dictionary.update(fields)
-        super().__init__(dictionary, status=self.STATUS)
-
-    @property
-    def locales(self):
-        """Returns the classes locales."""
         try:
-            return self.__class__.LOCALES[self.__class__.__name__]
+            message = self.__class__.locales[lang]  # Set by metaclass.
         except KeyError:
-            return {}
-
-    def message(self, lang, data=None):
-        """Returns the respective message."""
-        try:
-            message = self.locales[lang]
-        except KeyError:
-            return 'Could not get locale: {}.'.format(lang)
+            raise LanguageNotFound(lang)
 
         if data:
-            return message.format(*data)
+            message = message.format(*data)
 
-        return message
+        dictionary = {'message': message}
+        dictionary.update(fields)
+        super().__init__(dictionary, status=self.__class__.STATUS)

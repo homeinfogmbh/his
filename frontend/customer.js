6b720d0d258fc5239ba3fefbbe18fb4062fbebb0
Richard Neumann <r.neumann@homeinfo.de>
2018-02-28 17:09:41 +0100

    Started to implement customer.

---
+++ frontend/customer.js
@@ -0,0 +1,51 @@
+/*
+  customer.js - HOMEINFO Integrated Services customer library.
+
+  (C) 2017 HOMEINFO - Digitale Informationssysteme GmbH
+
+  This library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+  Maintainer: Richard Neumann <r dot neumann at homeinfo period de>
+
+  Requires:
+    * his.js
+*/
+"use strict";
+
+/*
+  HIS core namespace.
+*/
+var his = his || {};
+
+
+/*
+  HIS customer API.
+*/
+his.customer = his.customer || {};
+
+
+/*
+  Returns the respective customer URL.
+*/
+his.customer.getUrl = function (customerName) {
+  var url = his.BASE_URL + '/customer';
+
+  if (customerName != null) {
+    url += '/' + customerName;
+  }
+
+  return url;
+}
+
+

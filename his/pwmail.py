5e27d9e990a43bd305ed5ef5e6964f209e1e5e53
Richard Neumann <r.neumann@homeinfo.de>
2018-06-13 17:38:34 +0200

    Added Link as href.

--- his/pwmail.py
+++ his/pwmail.py
@@ -7,12 +7,22 @@ from his.config import CONFIG
 __all__ = ['mail_password_reset_link']
 
 
+HREF = '<a href="{}">{}</a>'
 PWRESET_CFG = CONFIG['pwreset']
 MAIL_CFG = CONFIG['mail']
 
 MAILER = Mailer(
     MAIL_CFG['host'], int(MAIL_CFG['port']), MAIL_CFG['user'],
     MAIL_CFG['passwd'])
+
+
+def href(url, caption=None):
+    """Makes a link."""
+
+    if caption is None:
+        caption = url
+
+    return HREF.format(url, caption)
 
 
 def mail_password_reset_link(password_reset_token):
@@ -23,7 +33,7 @@ def mail_password_reset_link(password_reset_token):
 
     account = password_reset_token.account
     link = PWRESET_CFG['link'].format(password_reset_token.token)
-    html = template.format(account=account.name, link=link)
+    html = template.format(account=account.name, link=href(link))
     email = EMail(
         PWRESET_CFG['subject'], MAIL_CFG['sender'], account.email,
         html=html)

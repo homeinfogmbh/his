0d5640bc9a04d05af5ed376655a83da161df78ef
Richard Neumann <r.neumann@homeinfo.de>
2018-06-20 13:02:56 +0200

    Refac.

--- install_locales.sh
+++ install_locales.sh
@@ -2,12 +2,16 @@
 #
 #  Install locales.
 #
+TARGET_BASE_DIR="/etc/his.d/locales"
+
 cd files/locales/ || exit 1
 
 for LANG in *; do
-	pushd "${LANG}/LC_MESSAGES" && {
+	pushd "${LANG}" && {
+		TARGET_DIR="${TARGET_BASE_DIR}/${LANG}/LC_MESSAGES"
+		mkdir -p "${TARGET_DIR}"
 		for PO in *.po; do
-			msgfmt "${PO}" -o "/etc/his.d/locales/${LANG}/LC_MESSAGES/${PO%.po}.mo"
+			msgfmt "${PO}" -o "${TARGET_DIR}/${PO%.po}.mo"
 		done
 		popd
 	}

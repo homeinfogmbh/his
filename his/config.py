1b1255c90dd4502ac8d4458f286dc4a6196031a2
Richard Neumann <r.neumann@homeinfo.de>
2018-06-29 10:45:57 +0200

    Made JSONParser alert.

--- his/config.py
+++ his/config.py
@@ -2,17 +2,10 @@
 
 from json import loads
 
-from configlib import INIParser
+from configlib import INIParser, JSONParser
 
 __all__ = ['CONFIG', 'RECAPTCHA']
 
 
 CONFIG = INIParser('/etc/his.d/his.conf')
-
-try:
-    with open('/etc/his.d/recaptcha.json') as file:
-        _JSON_TEXT = file.read()
-except FileNotFoundError:
-    RECAPTCHA = {}
-else:
-    RECAPTCHA = loads(_JSON_TEXT)
+RECAPTCHA = JSONParser('/etc/his.d/recaptcha.json', alert=True)

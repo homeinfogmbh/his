2a42ffeda5b4fa44003ca41d736a09adcf64532d
Richard Neumann <r.neumann@homeinfo.de>
2018-09-13 16:53:32 +0200

    --test

--- tests/backend/test_orm.py
+++ tests/backend/test_orm.py
@@ -14,7 +14,6 @@ class TestService(unittest.TestCase):
         self.assertIsInstance(Service.name, CharField)
         self.assertIsInstance(Service.description, CharField)
         self.assertIsInstance(Service.promote, BooleanField)
-        self.assertIsInstance(Service.foobar, dict)
 
 
 class TestAccount(unittest.TestCase):

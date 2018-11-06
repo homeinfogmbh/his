20fd7c515a63eb5e81a4a7a877e73ca6438a60a8
Richard Neumann <r.neumann@homeinfo.de>
2018-11-06 13:43:17 +0100

    Refac.

--- backend/his/messages/__init__.py
+++ backend/his/messages/__init__.py
@@ -1,18 +1,48 @@
 """HIS messages."""
 
-from his.messages.account import NoAccountSpecified, NoSuchAccount, \
-    AccountLocked, AccountCreated, AccountDeleted, AccountPatched, \
-    NotAuthorized, AccountExists, AccountsExhausted
+from his.messages.account import AccountCreated
+from his.messages.account import AccountDeleted
+from his.messages.account import AccountExists
+from his.messages.account import AccountLocked
+from his.messages.account import AccountPatched
+from his.messages.account import AccountsExhausted
+from his.messages.account import NoAccountSpecified
+from his.messages.account import NoSuchAccount
+from his.messages.account import NotAuthorized
 from his.messages.api import MessageNotFound, LanguageNotFound, Message
-from his.messages.customer import NoCustomerSpecified, NoSuchCustomer, \
-    CustomerUnconfigured
-from his.messages.data import DataError, MissingData, IncompleteData, \
-    InvalidData, NotAnInteger, InvalidCustomerID
-from his.messages.service import NoServiceSpecified, NoSuchService, \
-    ServiceAdded, ServiceAlreadyEnabled, AmbiguousServiceTarget, \
-    MissingServiceTarget
-from his.messages.session import MissingCredentials, InvalidCredentials, \
-    NoSessionSpecified, NoSuchSession, SessionExpired, DurationOutOfBounds
+from his.messages.customer import CustomerUnconfigured
+from his.messages.customer import NoCustomerSpecified
+from his.messages.customer import NoSuchCustomer
+from his.messages.data import DataError
+from his.messages.data import IncompleteData
+from his.messages.data import InvalidCustomerID
+from his.messages.data import InvalidData
+from his.messages.data import MissingData
+from his.messages.data import NotAnInteger
+from his.messages.pwreset import NoTokenSpecified
+from his.messages.pwreset import NoPasswordSpecified
+from his.messages.pwreset import PasswordResetSent
+from his.messages.pwreset import PasswordResetPending
+from his.messages.pwreset import InvalidResetToken
+from his.messages.pwreset import PasswordSet
+from his.messages.recaptcha import NoResponseProvided
+from his.messages.recaptcha import NoSiteKeyProvided
+from his.messages.recaptcha import SiteNotConfigured
+from his.messages.recaptcha import InvalidResponse
+from his.messages.request import RequestError, MissingContentType
+from his.messages.service import NoServiceSpecified
+from his.messages.service import NoSuchService
+from his.messages.service import ServiceAdded
+from his.messages.service import ServiceAlreadyEnabled
+from his.messages.service import AmbiguousServiceTarget
+from his.messages.service import MissingServiceTarget
+from his.messages.session import DurationOutOfBounds
+from his.messages.session import InvalidCredentials
+from his.messages.session import MissingCredentials
+from his.messages.session import NoSessionSpecified
+from his.messages.session import NoSuchSession
+from his.messages.session import SessionExpired
+
 
 __all__ = [
     'MessageNotFound',
@@ -39,6 +69,21 @@ __all__ = [
     'InvalidData',
     'NotAnInteger',
     'InvalidCustomerID',
+    # Password reset.
+    'NoTokenSpecified',
+    'NoPasswordSpecified',
+    'PasswordResetSent',
+    'PasswordResetPending',
+    'InvalidResetToken',
+    'PasswordSet',
+    # Recaptcha messages.
+    'NoResponseProvided',
+    'NoSiteKeyProvided',
+    'SiteNotConfigured',
+    'InvalidResponse',
+    # Request messages.
+    'RequestError',
+    'MissingContentType',
     # Service messages.
     'NoServiceSpecified',
     'NoSuchService',

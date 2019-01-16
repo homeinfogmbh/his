b9c7268106668777a6e7c81241863ac13f439c83
Richard Neumann <r.neumann@homeinfo.de>
2019-01-16 11:03:04 +0100

    Refac.

--- backend/his/crypto.py
+++ backend/his/crypto.py
@@ -1,24 +1,23 @@
 """HIS cryptography library."""
 
-try:
-    from random import choices
-except ImportError:
-    from random import choice
-
-    def choices(population, k=1):
-        """Behaves similar to python 3.6's random.choices."""
-
-        return [choice(population) for _ in range(k)]
-
-from string import ascii_letters, digits, punctuation
+from random import choice
+from string import ascii_letters, digits
 
 
 __all__ = ['genpw']
 
 
-def genpw(pool=ascii_letters+digits+punctuation, length=8):
-    """Generates a password with the specified
-    length from the character pool.
-    """
+POOL = ascii_letters + digits
 
-    return ''.join(choices(pool, k=length))
+
+def randchars(count, pool=POOL):
+    """Yields random chars from the pool."""
+
+    for _ in range(count):
+        yield choice(pool)
+
+
+def genpw(length=16, pool=POOL):
+    """Generates a safe, radom password."""
+
+    return ''.join(randchars(length, pool=pool))

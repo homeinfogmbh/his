c06396c5bd06360a2c83bf4db80b4f23fc958a8c
Richard Neumann <r.neumann@homeinfo.de>
2019-11-13 09:59:44 +0100

    Added multiple cookies for different domains.

--- backend/his/config.py
+++ backend/his/config.py
@@ -3,11 +3,11 @@
 from configlib import loadcfg
 
 
-__all__ = ['CONFIG', 'COOKIE', 'CORS', 'DOMAIN', 'RECAPTCHA']
+__all__ = ['CONFIG', 'COOKIE', 'CORS', 'DOMAINS', 'RECAPTCHA']
 
 
 CONFIG = loadcfg('his.d/his.conf')
 COOKIE = CONFIG['auth']['cookie']
-DOMAIN = CONFIG['auth']['domain']
+DOMAINS = CONFIG['auth']['domains'].split()
 CORS = loadcfg('his.d/cors.json')
 RECAPTCHA = loadcfg('his.d/recaptcha.json')
--- backend/his/functions.py
+++ backend/his/functions.py
@@ -1,6 +1,6 @@
 """Common functions."""
 
-from his.config import COOKIE, DOMAIN
+from his.config import COOKIE, DOMAINS
 from his.contextlocals import get_session
 from his.exceptions import NoSessionSpecified, SessionExpired
 
@@ -15,16 +15,20 @@ __all__ = [
 def set_session_cookie(response, session):
     """Sets the session cookie."""
 
-    response.set_cookie(
-        COOKIE, session.token.hex, expires=session.end, domain=DOMAIN,
-        secure=True)
+    for domain in DOMAINS:
+        response.set_cookie(
+            COOKIE, session.token.hex, expires=session.end, domain=domain,
+            secure=True)
+
     return response
 
 
 def delete_session_cookie(response):
     """Deletes the session cookie."""
 
-    response.delete_cookie(COOKIE, domain=DOMAIN)
+    for domain in DOMAINS:
+        response.delete_cookie(COOKIE, domain=domain)
+
     return response
 
 

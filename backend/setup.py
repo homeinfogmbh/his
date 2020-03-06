dd5bedfc1054fa3f4e68d1672c47fac9e9924637
Richard Neumann <r.neumann@homeinfo.de>
2020-03-06 14:53:22 +0100

    Added oauth2 subpackage.

--- backend/setup.py
+++ backend/setup.py
@@ -10,14 +10,17 @@ setup(
     requires=[
         'argon2', 'configlib', 'docopt', 'emaillib', 'filedb', 'flask',
         'functoolsplus', 'mdb', 'peewee', 'peeweeplus', 'recaptcha',
-        'werkzeug', 'wsgilib'],
+        'werkzeug', 'wsgilib'
+    ],
     packages=[
         'his',
         'his.hisutil',
         'his.messages',
+        'his.oauth2',
         'his.orm',
         'his.wsgi',
-        'his.wsgi.service'],
+        'his.wsgi.service'
+    ],
     scripts=['files/hisutil', 'files/his-session-cleanup'],
     data_files=[('/usr/local/etc/his.d', [
         'files/pwreset.html', 'files/bugreport.html'])],

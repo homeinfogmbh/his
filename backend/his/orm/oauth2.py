228e5b56e0452ef3f50effd2d5542113bd9bf7e6
Richard Neumann <r.neumann@homeinfo.de>
2020-03-06 17:09:59 +0100

    Started OAuth 2.0 implementation.

--- backend/his/orm/oauth2.py
+++ backend/his/orm/oauth2.py
@@ -1,1 +1,35 @@
 """OAuth 2.0 related models."""
+
+from peewee import ForeignKeyField
+
+from peeweeplus.authlib import OAuth2ClientMixin
+from peeweeplus.authlib import OAuth2TokenMixin
+from peeweeplus.authlib import OAuth2AuthorizationCodeMixin
+
+from his.orm.common import HISModel
+from his.orm.account import Account as User
+
+
+__all__ = ['Client', 'Token', 'AuthorizationCode']
+
+
+class Client(HISModel, OAuth2ClientMixin):   # pylint: disable=R0901
+    """An OAuth client."""
+
+    user = ForeignKeyField(
+        User, column_name='user', backref='clients', on_delete='CASCADE')
+
+
+class Token(HISModel, OAuth2TokenMixin):     # pylint: disable=R0901
+    """An OAuth bearer token."""
+
+    user = ForeignKeyField(
+        User, column_name='user', backref='tokens', on_delete='CASCADE')
+
+
+class AuthorizationCode(HISModel, OAuth2AuthorizationCodeMixin):
+    """An OAuth authorization code."""  # pylint: disable=R0901
+
+    user = ForeignKeyField(
+        User, column_name='user', backref='authorization_codes',
+        on_delete='CASCADE')

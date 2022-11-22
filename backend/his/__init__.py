578dc23c59708d7e45108c01318713eaa7c72a7a
Richard Neumann <r.neumann@homeinfo.de>
2022-11-22 13:15:56 +0100

    Add function stakeholders()

--- backend/his/__init__.py
+++ backend/his/__init__.py
@@ -7,6 +7,7 @@ from his.application import Application
 from his.config import get_cors
 from his.contextlocals import ACCOUNT, CUSTOMER, SESSION
 from his.crypto import genpw
+from his.functions import stakeholders
 from his.mail import get_mailer
 from his.orm import Account, AccountService, CustomerService, Service
 from his.parsers import account, service
@@ -29,5 +30,6 @@ __all__ = [
     'get_cors',
     'get_mailer',
     'root',
-    'service'
+    'service',
+    'stakeholders'
 ]
---
+++ backend/his/functions.py
@@ -0,0 +1,23 @@
+"""Miscellaneous functions."""
+
+from typing import Iterable, Union
+
+from his.orm import Account, AccountService, CustomerService, Service
+
+
+__all__ = ['stakeholders']
+
+
+def stakeholders(service: Union[Service, str]) -> Iterable[Account]:
+    """Select accounts that can use the given service."""
+
+    if isinstance(service, str):
+        service = Service.get(Service.name == service)
+
+    return Account.select().join(AccountService).join_from(
+        Account, CustomerService,
+        on=Account.customer == CustomerService.customer
+    ).where(
+        (AccountService.service == service)
+        & (CustomerService.service == service)
+    )
